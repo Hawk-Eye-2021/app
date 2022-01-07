fc61cd386b545071d3e9f870ce9c25263efa8482
mariano.longo <mariano.longo@ing.austral.edu.ar>
2022-01-07 18:31:01 -0300

    Fix de los compontents

--- client/src/content/pages/Themes/Detail/index.tsx
+++ client/src/content/pages/Themes/Detail/index.tsx
@@ -20,28 +20,33 @@ function ThemeDetail() {
                 spacing={3}
                 padding={3}
                 className={"gridContainer"}
+                container
             >
-                <Grid sm={12} md={8}>
+                <Grid sm={12} md={8} item>
                     <div className={"gridItem"}>
                         <Contents themeId={selectedTheme.id} themeName={selectedTheme.name}/>
                     </div>
                 </Grid>
-                <Grid sm={12} md={4}>
+                <Grid sm={12} md={4} item>
                     <div className={"gridItem"}>
-                        <iframe
-                            src={`https://hawk-eye-metabase.herokuapp.com/public/question/39949d2b-f27b-4013-a9c8-aeb5a33ddc25?theme_id=${selectedTheme.id}`}
-                            frameBorder="0"
-                            height={"50%"}
-                            width={"100%"}
-                            allowTransparency
-                        />
-                        <iframe
-                            src={`https://hawk-eye-metabase.herokuapp.com/public/question/39949d2b-f27b-4013-a9c8-aeb5a33ddc25?theme_id=${selectedTheme.id}`}
-                            frameBorder="0"
-                            height={"50%"}
-                            width={"100%"}
-                            allowTransparency
-                        />
+                        <div className={"graph-wrapper"}>
+                            <iframe
+                                src={`https://hawk-eye-metabase.herokuapp.com/public/question/39949d2b-f27b-4013-a9c8-aeb5a33ddc25?theme_id=${selectedTheme.id}`}
+                                frameBorder="0"
+                                height={"100%"}
+                                width={"100%"}
+                                allowTransparency
+                            />
+                        </div>
+                        <div className={"graph-wrapper"}>
+                            <iframe
+                                src={`https://hawk-eye-metabase.herokuapp.com/public/question/39949d2b-f27b-4013-a9c8-aeb5a33ddc25?theme_id=${selectedTheme.id}`}
+                                frameBorder="0"
+                                height={"100%"}
+                                width={"100%"}
+                                allowTransparency
+                            />
+                        </div>
                     </div>
                 </Grid>
             </Grid>
